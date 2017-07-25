301ac406fbc271045e04c07ae7f3ae15ea538d29
Arissan <arissetyawan.email@gmail.com>
2017-07-25 12:11:11 +0700

    reactjs

--- app/assets/javascripts/components/event_application.js.jsx
+++ app/assets/javascripts/components/event_application.js.jsx
@@ -17,12 +17,28 @@ var EventApplication = React.createClass({
       }
     });
   },
+  handleSearch: function(events) {
+      this.setState({ events: events });
+    },
+  handleAdd: function(event) {
+      var events = this.state.events;
+      events.push(event);
+      this.setState({ events: events });
+    },
   render: function() {
     return(
       <div className="container">
         <div className="jumbotron">
           <h1>ReactJS Tutorial</h1>
-          <p>by Piotr Jaworski</p>
+          <p>by arissetyawan</p>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <SearchForm handleSearch={this.handleSearch} />
+          </div>
+          <div className="col-md-8">
+            <NewForm handleAdd={this.handleAdd} />
+          </div>
         </div>
         <div className="row">
           <div className="col-md-12">
@@ -32,4 +48,6 @@ var EventApplication = React.createClass({
       </div>
     )
   }
+
+
 });
